1e45d983eec50d9480c69bf994a14a6976250568
viktorpohribnyi <pogribnijviktor4@gmail.com>
2024-12-14 10:27:31 +0200

    Add project files.

---
+++ FileDownloadApp/Controllers/FileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace FileDownloadApp.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet("")]
+        [HttpGet("File/DownloadFile")]
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost("File/DownloadFile")]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                fileName = "output.txt";
+            }
+
+            string fileContent = $"Ім'я: {firstName}\nПрізвище: {lastName}";
+            byte[] fileBytes = Encoding.UTF8.GetBytes(fileContent);
+
+            return File(fileBytes, "text/plain", fileName);
+        }
+    }
+}
